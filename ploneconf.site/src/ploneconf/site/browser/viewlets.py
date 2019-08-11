1702d7915d2d46bc5d4110d6531934c8ef2111fe
wurky <694335199@qq.com>
2019-08-11 21:23:03 +0800

    25 Exercise 2

--- ploneconf.site/src/ploneconf/site/browser/viewlets.py
+++ ploneconf.site/src/ploneconf/site/browser/viewlets.py
@@ -1,8 +1,22 @@
 from plone.app.layout.viewlets import ViewletBase
-from ploneconf.site.behaviors.social import ISocial
+from datetime import datetime
+import arrow
 
+CONFERENCE_START_DATE = datetime(2015, 10, 12)
+
+
+class DaysToConferenceViewlet(ViewletBase):
+
+    def date(self):
+        return CONFERENCE_START_DATE
+
+    def human(self):
+        return arrow.get(CONFERENCE_START_DATE).humanize()
+# from ploneconf.site.behaviors.social import ISocial
+#
 class SocialViewlet(ViewletBase):
+    pass
 
-    def lanyrd_link(self):
-        adapted = ISocial(self.context)
-        return adapted.lanyrd
+#     def lanyrd_link(self):
+#         adapted = ISocial(self.context)
+#         return adapted.lanyrd
